7c0ce96534a073de0bf8fd29468acf5c260ad151
sisay82 <sisay82@gmail.com>
2015-06-29 12:38:04 +0200

    Added exercise project with test file and code implemented

---
+++ Utilities/Utilities/Utility.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Utility
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        ///  Fills an array of size N with the numbers from 1 to N in random order without duplicates
+        /// </summary>
+        /// <param name="arrayToFill">Array To fill</param>
+        public static void RandomArrayFill(Object[] arrayToFill)
+        {
+
+            int n = arrayToFill.Length;
+
+            Int32 currentNum = Int32.MinValue;
+            Random randNum = new Random();
+
+            //Input cleaning
+            for (int i = 0; i < n; i++)
+            {
+                arrayToFill[i] = Int32.MinValue;
+            }
+
+            //Array filled by rondom number form 1 to n
+            for (int i = 0; i < n; i++)
+            {
+                currentNum = randNum.Next(0, n) + 1;
+
+                while (arrayToFill.Contains(currentNum))
+                {
+                    currentNum = randNum.Next(0, n) + 1;
+                }
+
+                arrayToFill[i] = currentNum;
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Encode every letter into its corresponding numbered position in alphabet
+        /// </summary>
+        /// <param name="value">String to encode</param>
+        /// <returns>Encoded string</returns>
+        public static String GetLatinLetterEncoded(String value)
+        {
+            String result = String.Empty;
+
+            foreach (Char c in value.ToUpper())
+            {
+                if (c < 'A' || c > 'Z')
+                {
+                    result += c;
+                }
+                else
+                {
+                    result += (Convert.ToInt32(c) - Convert.ToInt32('A') + 1).ToString();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// The brackets are correctly matched and each one is accounted for. Only round brackets are allowed.
+        /// </summary>
+        /// <param name="value">String to check</param>
+        /// <returns></returns>
+        public static Boolean CheckBackets(String value)
+        {
+            Stack<Char> bracketsStack = new Stack<char>(value.Length);
+            foreach (Char c in value)
+            {
+                if (c == '(')
+                    bracketsStack.Push(c);
+
+                if (c == ')')
+                {
+                    if (bracketsStack.Count == 0) return false;
+
+                    bracketsStack.Pop();
+                }
+            }
+
+            if (bracketsStack.Count > 0) return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ Utilities/Utilities/UtilityTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Utilities
+{
+    [TestFixture]
+    public class RandomArrayFillTest : AssertionHelper
+    {
+        Object[] ints = new Object[20];
+
+        [SetUp]
+        public void Init()
+        {
+            Utility.RandomArrayFill(ints);
+        }
+
+        [Test]
+        public void Check01NotNullElements()
+        {
+            Assert.That(ints, Is.All.Not.Null);
+        }
+
+        [Test]
+        public void Check02IntTypeElements()
+        {
+            Assert.That(ints, Is.All.InstanceOf(typeof(int)));
+        }
+        
+        [Test]
+        public void Check03UniqueElements()
+        {
+            Assert.That(ints, Is.Unique);
+        }
+
+        [Test]
+        public void Check04ValuesRange()
+        {
+            Assert.That(ints, Is.All.GreaterThan(0));
+            Assert.That(ints, Is.All.LessThanOrEqualTo(ints.Length));
+        }
+
+    }
+
+    [TestFixture]
+    public class LatinLetterEncodingTest : AssertionHelper
+    {
+        [TestCase("af5c a#!", "1653 1#!")]
+        [TestCase("hello 45", "85121215 45")]
+        [TestCase("jaj-a", "10110-1")]
+        public void CheckEncodedString(String input, String output)
+        {
+            Assert.That(Utility.GetLatinLetterEncoded(input), Is.EqualTo(output));
+        }
+    }
+
+    [TestFixture]
+    public class BracketCheckTest : AssertionHelper
+    {
+        [TestCase("afdoi$%dw", true)]
+        [TestCase("(hello (world))", true)]
+        [TestCase("((hello (world))", false)]
+        [TestCase("(c(oder)) b(yte)", true)]
+        [TestCase("(coder))byte(", false)]
+        public void CheckCorrectBracket(String input, Boolean result)
+        {
+            Assert.That(Utility.CheckBackets(input) == result);
+        }
+    }
+}
